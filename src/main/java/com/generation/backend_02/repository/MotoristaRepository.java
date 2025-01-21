65c4ef63520f382c1c1a1be71bbc5aec85c16fbb
julianaMonteiro <juliana47monteiro@gmail.com>
2025-01-21 14:30:10 -0300

    criando o repository

---
+++ src/main/java/com/generation/backend_02/repository/MotoristaRepository.java
@@ -0,0 +1,13 @@
+package com.generation.backend_02.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+
+import com.generation.backend_02.model.Motorista;
+
+public interface MotoristaRepository extends JpaRepository<Motorista,Long>{
+
+	public List <Motorista> findAllByNomeContainingIgnoreCase(@Param("nome") String nome);
+}
