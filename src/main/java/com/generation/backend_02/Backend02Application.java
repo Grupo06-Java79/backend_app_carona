be9f694a5a089222e8d3750352187672e3c7427a
Guilherme Kaludin <guikaludin@outlook.com>
2025-01-21 11:00:54 -0300

    Primeiro commit

---
+++ src/main/java/com/generation/backend_02/Backend02Application.java
@@ -0,0 +1,13 @@
+package com.generation.backend_02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Backend02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Backend02Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/generation/backend_02/Backend02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.backend_02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Backend02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
